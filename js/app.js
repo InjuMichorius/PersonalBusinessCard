e8c69cae206edd4b537eb4943a6a6dd362f3cb8d
inju <inju.michorius@hva.nl>
2021-02-01 14:55:25 +0100

    static website

--- js/app.js
+++ js/app.js
@@ -2,6 +2,11 @@ fetch("https://600ff44f6c21e1001704fac2.mockapi.io/minor-web/api/squads/1/teams/
 .then(response => response.json())
 .then(data => console.log(data));
 
+
+
+function ou() {
+    alert("He!")
+  }
 console.log('Ga mijn console eens uit')
 
 document.querySelector('#data').innerHTML = data;
