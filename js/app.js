ae4ed4f1bcfcf17e90d40394df3644d134452fda
inju <inju.michorius@hva.nl>
2021-02-01 14:34:37 +0100

    Added static site

---
+++ js/app.js
@@ -0,0 +1,7 @@
+fetch("https://600ff44f6c21e1001704fac2.mockapi.io/minor-web/api/squads/1/teams/2/members")
+.then(response => response.json())
+.then(data => console.log(data));
+
+console.log('Ga mijn console eens uit')
+
+document.querySelector('#data').innerHTML = data;
